0c4837c4de5058a5c7329b228a32b9bc9048031c
Einstephener <kihyuk0201@gmail.com>
2024-01-02 20:14:40 +0900

    [feat] 박기혁 / EASY FPS 에셋 추가
    FPS에셋 추가
    총기 에셋 추가

---
+++ Assets/Script/Item/GunData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GunType
+{
+    Pistol,
+    Rifle,
+    Shotgun,
+    Sniper
+}
+public enum GunStyle
+{
+    nonautomatic, 
+    automatic
+}
+[CreateAssetMenu(fileName = "Item", menuName = "New Gun")]
+public class GunData : ScriptableObject
+{
+    [Header("Info")]
+    public string gunName;
+    public string description;
+    public GunType gunType;
+    public GunStyle gunStyle;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public int bulletInTheGun; //탄알집에 남아있는 탄알
+    public int maxBulletInTheGun; //탄알집에 최대로 채울 수 있는 수
+    public int maxBulletAmount; //소유중인 탄알 수
+}
---
+++ Assets/Script/Item/ItemObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour 
+{
+    public GunData item;
+
+    public string GetInteractPrompt()
+    {
+        return string.Format("Pickup {0}", item.gunName);
+    }
+
+    public void OnInteract()
+    {
+        Destroy(gameObject);
+    }
+}
