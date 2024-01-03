036f4d8e789f6d0172c514c583e875feb924d400
Einstephener <kihyuk0201@gmail.com>
2024-01-03 14:28:25 +0900

    [feat] 박기혁 / 무기 레이어 추가

--- Assets/Script/Item/GetTheGun.cs
+++ Assets/Script/Item/GetTheGun.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class GetTheGun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public GameObject currentGun; 
 
-    // Update is called once per frame
-    void Update()
+    private int currentGunCounter = 0;
+
+    public List<string> gunsIHave = new List<string>();
+
+    public float switchWeaponCooldown;
+
+
+    //이미 소지하고 있는 무기 선택
+    void Create_Weapon()
     {
-        
+        /*
+		 * Scrolling wheel waepons changing
+		 */
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            switchWeaponCooldown = 0;
+
+            currentGunCounter++;
+            if (currentGunCounter > gunsIHave.Count - 1)
+            {
+                currentGunCounter = 0;
+            }
+            StartCoroutine("Spawn", currentGunCounter);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            switchWeaponCooldown = 0;
+
+            currentGunCounter--;
+            if (currentGunCounter < 0)
+            {
+                currentGunCounter = gunsIHave.Count - 1;
+            }
+            StartCoroutine("Spawn", currentGunCounter);
+        }
+
+        /*
+		 * Keypad numbers
+		 */
+        if (Input.GetKeyDown(KeyCode.Alpha1) && currentGunCounter != 0)
+        {
+            switchWeaponCooldown = 0;
+            currentGunCounter = 0;
+            StartCoroutine("Spawn", currentGunCounter);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && currentGunCounter != 1)
+        {
+            switchWeaponCooldown = 0;
+            currentGunCounter = 1;
+            StartCoroutine("Spawn", currentGunCounter);
+        }
+
     }
 }
+
