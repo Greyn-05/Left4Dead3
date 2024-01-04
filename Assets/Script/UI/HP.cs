7d06dea35b8421938fa0b756a73809e96841790b
a-ou-d <148048020+a-ou-d@users.noreply.github.com>
2024-01-04 15:09:17 +0900

    [DESIGN] 김세연 / MISSION, HP BAR UI
    미션, 체력바 디자인은 끝 스크립트만 달면 끝남
    총하고 총알은 일단 위치만 판넬로 잡아둠

---
+++ Assets/Script/UI/HP.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    // 체력바 업데잍느 함수
+    // private void UpdateHP()
+    // {
+    //     hpSlider.value = Mathf.Lerp(hpSlider.value, CurrentHP, Time.deltaTime * 10);
+    // }
+}
