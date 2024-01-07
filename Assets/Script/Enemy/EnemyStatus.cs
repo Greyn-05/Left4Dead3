14410562b0110dbad8ba96d57b5339ae1bea37ff
Noname136-rpg <zofhfh2323@gmail.com>
2024-01-07 18:43:03 +0900

    [feat] 김호영 zombie 피격, 죽음 구현

--- Assets/Script/Enemy/EnemyStatus.cs
+++ Assets/Script/Enemy/EnemyStatus.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class EnemyStatus : MonoBehaviour
 {
-    [Header("Walk, Run")]
+    [Header("Enemy Status")]
     [SerializeField]
     private float walkSpeed;
     [SerializeField]
     private float runSpeed;
     [SerializeField]
-    private float attackDamage;
-    [SerializeField]
-    private float defence;
+    private int attackDamage;
 
     public float WalkSpeed => walkSpeed;
     public float RunSpeed => runSpeed;
-    public float AttackDamage => attackDamage;
-    public float Defence => defence;
+    public int AttackDamage => attackDamage;
+
 }
