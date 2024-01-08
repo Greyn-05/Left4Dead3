6d74239d67b0ee6c88ef4822a905d655733a9817
Einstephener <kihyuk0201@gmail.com>
2024-01-08 15:01:51 +0900

    [feat] 문닫기 애니메이션 추가

--- Assets/Script/Controller/DoorInfo.cs
+++ Assets/Script/Controller/DoorInfo.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class DoorInfo : MonoBehaviour, IOpenDoor
 {
     private Animator door_Animator;
+
+    public bool IsOpen { get ; set; }
+
     private void Awake()
     {
         door_Animator = GetComponent<Animator>();
+        IsOpen = false;
     }
     public void OpenThisDoor()
     {
         door_Animator.SetTrigger("Open");
+        IsOpen = true;
+    }
+    public void CloseThisDoor()
+    {
+        door_Animator.SetTrigger("Close");
+        IsOpen = false;
     }
 }
